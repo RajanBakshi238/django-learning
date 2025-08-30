c9266f2e6137afab91474f8d8e17efb95c8b22f2
RajanBakshi238 <rajan.bakshi238@gmail.com>
2025-08-31 02:14:41 +0530

    Add login functionality with authentication support

--- user/views.py
+++ user/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from .serializers import UserSignupSerializer
+from .serializers import UserLoginSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
+from django.contrib.auth import authenticate
 class SignupView(APIView):
     @swagger_auto_schema(request_body=UserSignupSerializer) 
     def post(self, request):
@@ -15,3 +17,17 @@ class SignupView(APIView):
                 'token': token.key
             }, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    @swagger_auto_schema(request_body=UserLoginSerializer)
+    def post(self, request):
+        serializer = UserLoginSerializer(data = request.data)
+        if serializer.is_valid():
+            user = authenticate(email=serializer.validated_data['email'], password=serializer.validated_data['password'])
+            if user is not None:
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({
+                    'token': token.key
+                }, status=status.HTTP_200_OK)
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
